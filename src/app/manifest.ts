47187f2915febf81add5ead40f387a03ccd0fd83
9 <hieutran13091988@gmail.com>
2026-06-06 09:33:27 +0000

    Lấy này làm icon ứng dụng khi depploy trên màn hình di động

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,13 +13,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0054A4',
     icons: [
       {
-        src: 'https://placehold.co/192x192/0054A4/FFFFFF?text=DUE',
+        src: 'https://placehold.co/192x192/FFFFFF/0054A4?text=DUE+1975',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable'
       },
       {
-        src: 'https://placehold.co/512x512/0054A4/FFFFFF?text=DUE',
+        src: 'https://placehold.co/512x512/FFFFFF/0054A4?text=DUE+SINCE+1975',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any'
