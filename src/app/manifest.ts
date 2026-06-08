9009eb11b83881b545d9ccb1c42a0d62ef985bb2
9 <hieutran13091988@gmail.com>
2026-06-08 00:45:32 +0000

    Fix icon ứng dụng DUE vừa khớp icon đẹp hơn

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,8 +1,9 @@
+
 import { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   // Biểu tượng SVG DUE chuẩn: D (Cam #F58220), U (Xanh lá #009E49), E (Xanh dương #0054A4) trên nền trắng
-  const iconSvg = `data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNTEyIiBoZWlnaHQ9IjUxMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSIjRkZGRkZGIi8+PHRleHQgeD0iNTAlIiB5PSI1MCUiIGZvbnQtZmFtaWx5PSJJbnRlciwgc2Fucy1zZXJpZiIgZm9udC13ZWlnaHQ9IjkwMCIgZm9udC1zaXplPSIyODAiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGRvbWluYW50LWJhc2VsaW5lPSJjZW50cmFsIj48dHNwYW4gZmlsbD0iI0Y1ODIyMCI+RDwvdHNwYW4+PHRzcGFuIGZpbGw9IiMwMDlFNDkiPlU8L3RzcGFuPjx0c3BhbiBmaWxsPSIjMDA1NEE0Ij5FPC90c3Bhbj48L3RleHQ+PC9zdmc+`;
+  const iconSvg = `data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNTEyIiBoZWlnaHQ9IjUxMiIgdmlld0JveD0iMCAwIDUxMiA1MTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjUxMiIgaGVpZ2h0PSI1MTIiIGZpbGw9IiNGRkZGRkYiLz48dGV4dCB4PSI1MCUiIHk9IjUyJSIgZm9udC1mYW1pbHk9IidJbnRlcicsIHNhbnMtc2VyaWYiIGZvbnQtd2VpZ2h0PSI5MDAiIGZvbnQtc2l6ZT0iMjQwIiB0ZXh0LWFuY2hvcj0ibWlkZGxlIiBkb21pbmFudC1iYXNlbGluZT0ibWlkZGxlIiBsZXR0ZXItc3BhY2luZz0iLTEwIj48dHNwYW4gZmlsbD0iI0Y1ODIyMCI+RDwvdHNwYW4+PHRzcGFuIGZpbGw9IiMwMDlFNDkiPlU8L3RzcGFuPjx0c3BhbiBmaWxsPSIjMDA1NEE0Ij5FPC90c3Bhbj48L3RleHQ+PC9zdmc+`;
 
   return {
     name: 'Requisition form DUE',
